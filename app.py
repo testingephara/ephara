a2afd5d3c9186b6324579afdce3229e0c38e59bb
Reza Banitaba <ahmadreza.banitaba@gmail.com>
2019-04-04 22:09:54 -0700

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def get_message(msg):
     except:
         response = "I don't understand that special symbol"
     # return selected item to the user
-    return (response)
+    return (response + output)
 
     
 #uses PyMessenger to send response to user
