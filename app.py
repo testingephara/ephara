be0e35b128eb8c8925d06acc356f0264eeb93179
Reza Banitaba <ahmadreza.banitaba@gmail.com>
2019-04-04 22:40:29 -0700

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ def get_message(msg):
             response = "Please be polite to me :("
         elif re.search("give me a random number", userinput):
             random_num= random.randint(1, 10)
-            resonse = "here is your random number" + random_num
+            resonse = "here is your random number" + str(random_num)
         else:
             response = "Sorry, I didn't understand what you said" + output
             
