094d668218fc56e721382881d2ba1f957dfc5140
Reza Banitaba <ahmadreza.banitaba@gmail.com>
2019-04-05 01:34:26 -0700

    Update app.py

--- app.py
+++ app.py
@@ -56,9 +56,6 @@ def get_message(msg):
         userinput = msg.lower()
         list=[]
         list.append(userinput)
-        chatLog
-        for i in list:
-            chatLog += i
         if re.search("^hi", userinput) or re.search("hello", userinput):
             response = "hello, I am Ephara! I can give you directions and basic information about any topic!"
         elif re.search("^where is", userinput):
@@ -93,7 +90,7 @@ def get_message(msg):
             response = "Please be polite to me :("
         elif re.search("give me a random number", userinput):
             random_num = randint(0, 100)
-            response = "here is your random number " + str(random_num) + chatLog
+            response = "here is your random number " + str(random_num)
         else:
             response = "Sorry, I didn't understand what you said"
             
