7449c63bc2ae44c0fd27ef85e4f2dbb8fe89cea0
euug <eugenejoy12@gmail.com>
2019-04-05 01:05:34 -0700

    Update app.py

--- app.py
+++ app.py
@@ -65,9 +65,9 @@ def get_message(msg):
             print(location)
             response = ("Click the link below!\nwww.google.com/maps/search/%s" % location)
         elif re.search("^what is", userinput) or re.search("^define", userinput):
-            thing = re.split("what is ", userinput, 2)
+            thing = re.split("what is ", userinput, 1)
             print(thing)
-            num_page = 0
+            num_page = 1
             search_results = google.search(thing[1], num_page)
             response = search_results[0].description
         elif re.search("^bye", userinput) or re.search("^goodbye", userinput):
